c1be07b9598b476ca824283645e1d13c428e615f
gogi999 <g.petrovic9@gmail>
2021-03-25 23:03:18 +0100

    Added ex03/src/index.js

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+function* myGenerator(...args) {
+    yield* [1, 2, 3, 4, 5];
+    yield* 'Arena';
+    yield* args;
+}
+
+let iterator = myGenerator(6, 7, 8);
+let generatorArray = [];
+
+for (let value of iterator) {
+    generatorArray.push(value)
+}
+
+console.log(generatorArray);
+// Only change code above this line
+module.exports = { generatorArray, myGenerator};
